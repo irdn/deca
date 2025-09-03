22976c709c50ad67d99f7c9243d6168f82500582
Mamali <devmoal@gmail.com>
2025-09-03 21:28:59 +0330

    Config updated

--- next.config.ts
+++ next.config.ts
@@ -6,8 +6,6 @@ const nextConfig: NextConfig = {
       {
         protocol: 'https',
         hostname: 'randomuser.me',
-        port: '',
-        pathname: '/api/portraits/**',
       },
     ],
   },
